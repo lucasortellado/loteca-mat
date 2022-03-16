b119ace79eecc4a6697ce49bdf9dab595a6062a4
lucasortellado <lucasortelladosilveira10@gmail.com>
2022-03-16 09:54:58 -0400

    jj

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-var numSort = [];
-var numEsco = [];
-
+var numSort = []
+var numEsco = []
 
 function sorteio() {
+    var cont = 0
+    numSort = []
+
     while (cont < 6) {
-        let num = Math.random() * 60;
-        num = Math.ceil(num);
+        let num = Math.random() * 60
+        num = Math.ceil(num)
         if (!numSort.includes(num)) {
-            numSort[cont] = num;
-            console.log(numSort);
-            cont++;
+            numSort[cont] = num
+            console.log(numSort)
+            cont++
         }
 
     }
 
-
+    document.getElementById("sorteados").innerHTML = numSort
+    contAcertos()
 }
 
 function getValor(valor, pos) {
-    valor = Number(valor);
-    if (valor <= 0 || valor >= 60) {
-        alert("Numero invalido, digite um número entre 1 e 60");
+    valor = Number(valor)
+    if (valor <= 0 || valor > 60) {
+        alert("Numero invalido, digite um entre 1 e 60")
+        document.getElementById(`num${pos + 1}`).value = ""
     } else if (numEsco.includes(valor)) {
-        alert("Número repetido, escolha um outro número!");
-
-    }else{
-        numEsco[pos] =valor;
-        console.log(numEsco);
+        alert("Numero repetido, escolha um outro numero!")
+        document.getElementById(`num${pos + 1}`).value = ""
+    } else {
+        numEsco[pos] = valor
+        console.log(numEsco)
     }
+
+}
+
+function contAcertos() {
+    var contA = 0
+    numEsco.forEach(function (value, index) {
+        if (numSort.includes(value)) {
+            contA = contA + 1
+        }
+    })
+    document.getElementById("acertos").innerHTML = contA
 }
